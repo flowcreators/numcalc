77d3b47d49c97584210315afda731a5a475f51d3
Dan Toruno <macbookpro@Toruno-Mac.local>
2025-09-20 10:48:08 -0400

    Update Next.js config with correct basePath and add .nojekyll

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: '',
-  assetPrefix: '',
+  basePath: '/numcalc',
+  assetPrefix: '/numcalc/',
   trailingSlash: true,
 };
 
